9f79db376207e39f18224402046682733d3e855f
Gentil Silva <silvagentil.sg@gmail.com>
2023-05-21 16:14:29 -0300

    Criando interface para validar e melhorar codigo. Aplicando os princípios SOD do SOLID

--- api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,11 +1,14 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultas {
@@ -19,6 +22,11 @@ public class AgendaDeConsultas {
     @Autowired
     private PacienteRepository pacienteRepository;
 
+//  Ao injetar uma interface o spring busca todas as classes que a implementa e cria uma lista com elas automáticamente.
+//  Podendo passar por todas as validações utilizando um loop. Sem precisar chamar classe por classe.
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores;
+
     public void agendar(DadosAgendamentoConsulta dados) {
         if(!pacienteRepository.existsById(dados.idPaciente())) {
             throw new ValidacaoException("Id do paciente informado não existe!");
@@ -27,6 +35,8 @@ public class AgendaDeConsultas {
         if(dados.idMedico()!= null && !medicoRepository.existsById(dados.idMedico())) {
             throw  new ValidacaoException("Id do medico informado não existe!");
         }
+
+        validadores.forEach(v -> v.validar(dados));
 
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
@@ -42,9 +52,7 @@ public class AgendaDeConsultas {
         if(dados.especialidade() == null) {
             throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido!");
         }
-
         return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
-
     }
 
 }
